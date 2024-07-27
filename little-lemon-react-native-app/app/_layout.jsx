5a63983e145db2bf8821b05c1a9d38b11d6a4665
Alexandre Bevilacqua <alexandrebba4@hotmail.com>
2024-07-27 10:08:14 +0200

    Commit: Setting up fonts with tailwind css and added assets.

--- little-lemon-react-native-app/app/_layout.jsx
+++ little-lemon-react-native-app/app/_layout.jsx
@@ -1,8 +1,24 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { Stack } from "expo-router"
+import React, { useEffect } from 'react'
+import { SplashScreen, Stack } from 'expo-router'
+import { useFonts } from 'expo-font'
+
+SplashScreen.preventAutoHideAsync();
 
 const RootLayout = () => {
+  const [FontsLoaded, error] = useFonts({
+    "MarkaziText-Medium": require("../assets/fonts/MarkaziText-Medium.ttf"),
+    "Karla-Regular": require("../assets/fonts/Karla-Regular.ttf")
+  })
+
+  useEffect(() => {
+    if(error) throw error;
+
+    if(FontsLoaded) SplashScreen.hideAsync();
+  }, [FontsLoaded, error])
+
+  if(!FontsLoaded && !error) return null;
+
   return (
     <Stack>
         <Stack.Screen name="index" options={{ headerShown: false}}>
