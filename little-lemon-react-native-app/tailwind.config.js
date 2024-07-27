ba8c4b14c46e34d6b055ff5b5093e09c30efe449
Alexandre Bevilacqua <alexandrebba4@hotmail.com>
2024-07-27 10:08:14 +0200

    Commit: Setting up fonts with tailwind css and added assets.

--- little-lemon-react-native-app/tailwind.config.js
+++ little-lemon-react-native-app/tailwind.config.js
@@ -2,8 +2,39 @@
 module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./Components/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          green: '#49A357', // Corrected color value
+          yellow: '#F4CE14',
+        },
+        secondary: {
+          pink: '#EE9972',
+          pale: '#FBDABB',
+        },
+        highlight: {
+          lightGray: '#EDEFEE',
+          darkGray: '#333333',
+        },
+      },
+      fontSize: {
+        'subtitle': '2.5rem', // 40px
+        'medium': '1.125rem', // 18px
+        'card-title': '1.125rem', // 18px
+        'paragraph': '1rem', // 16px
+        'highlight': '1rem', // 16px
+      },
+      lineHeight: {
+        'default': '1.5',
+      },
+      borderRadius: {
+        'default': '1rem',
+      },
+      fontFamily: {
+        'markazi-medium': ['MarkaziText-medium', 'serif'],
+        'karla-regular': ['Karla-Regular', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
-
