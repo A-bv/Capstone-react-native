e3f185ec6de7b8c5e060e114b4b4ba32ee59f195
Alexandre Bevilacqua <alexandrebba4@hotmail.com>
2024-07-27 12:58:01 +0200

    Commit: Tab bar styling

--- little-lemon-react-native-app/app/(tabs)/_layout.jsx
+++ little-lemon-react-native-app/app/(tabs)/_layout.jsx
@@ -4,15 +4,14 @@ import { Tabs } from 'expo-router';
 import { icons } from "../../constants";
 
 const TabIcon = ({ icon, color, name, focused }) => {
-    const iconSize = focused ? 28 : 24; // Increase icon size when focused
-    const labelSize = focused ? 14 : 12; // Increase label size when focused
+    const iconSize = focused ? 26 : 24; // Increase icon size when focused
 
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', gap: 2 }}>
         <Image
           source={icon}
           resizeMode="contain"
-          style={{ width: iconSize, height: iconSize, tintColor: color }} // Adjust icon size based on focus
+          style={{ width: iconSize, height: iconSize, tintColor: color }}
         />
       </View>
     );
@@ -22,13 +21,13 @@ const TabsLayout = () => {
     return (
         <Tabs
             screenOptions={{
-                tabBarActiveTintColor: "#F4CE14", // primary yellow
-                tabBarInactiveTintColor: "#CDCDE0", // inactive color
-                tabBarShowLabel: true, // Disable default labels
+                tabBarActiveTintColor: "#F4CE14",
+                tabBarInactiveTintColor: "#CDCDE0",
+                tabBarShowLabel: true,
                 tabBarStyle: {
-                    backgroundColor: "#495E57", // primary green
-                    borderTopWidth: 1,
-                    borderTopColor: "#228B22", // Dark green
+                    backgroundColor: "#495E57",
+                    borderTopWidth: 3,
+                    borderTopColor: "#495E57",
                     height: 84,
                 },
             }}
