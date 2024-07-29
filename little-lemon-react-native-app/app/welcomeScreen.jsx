446af3a409c2a6ddb30df5ed1a28013f2137761d
Alexandre Bevilacqua <alexandrebba4@hotmail.com>
2024-07-29 10:21:43 +0200

    Commit: refactor index.jsx entry point

---
+++ little-lemon-react-native-app/app/welcomeScreen.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { ScrollView, Text, Image, View } from 'react-native';
+import { router, Link } from 'expo-router';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { images } from "../constants";
+import CustomButton from '../components/CustomButton';
+
+const WelcomeScreen = () => {
+  return (
+<SafeAreaView className="bg-primary-green h-full">
+      <ScrollView
+        contentContainerStyle={{
+          height: "100%",
+        }}
+      >
+        <View className="w-full flex justify-center items-center min-h-[85vh] px-4">
+          <Image
+            source={images.cards}
+            className="w-[300px] h-[300px]"
+            resizeMode="contain"
+          />
+
+          <Text className="text-3xl text-white font-bold text-center font-markazi-medium">
+            Welcome to Little Lemon!{"\n"}
+            Fresh &{" "}
+            <Text className="text-secondary-200 text-primary-yellow">Delicious</Text>
+          </Text>
+
+          <CustomButton
+            title="Continue with Email"
+            handlePress={() => router.push("/sign-in")}
+            containerStyles="w-full mt-7"
+          />
+
+          <View className="flex justify-center pt-5 flex-row gap-2">
+            <Text className="text-lg text-gray-100 font-pregular">
+              Maybe later?
+            </Text>
+            <Link
+              href="/home"
+              className="text-lg font-psemibold text-secondary"
+            >
+              Continue
+            </Link>
+          </View>
+        </View>
+      </ScrollView>
+      <StatusBar backgroundColor="#161622" style="light" />
+    </SafeAreaView>
+  );
+}
+
+export default WelcomeScreen;
